490d24c42c9052481513397b07eb2a39f3659012
Robert Schlaeger <RobertSchlaeger@hotmail.com>
2020-10-22 21:02:21 +0200

    initial commit

---
+++ src/components/SignalR.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/sr-logo.png')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <p>Here be messages:</p>
+        <div class="message-wrapper">
+          <ul >
+            <li v-for="message in messages" :key="message">
+               {{ message }} 
+            </li>
+          </ul>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <div>
+          <v-input ><v-text-field label="Please enter a message"> </v-text-field></v-input>
+        </div>
+      </v-col>
+    </v-row>
+
+  </v-container>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'SignalR',
+    data: () => ({
+      messages: ["1","2"],
+    }),
+  })
+</script>
